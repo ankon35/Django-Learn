a0e283483a076cfa9082c2d1fb6b064e14855dce
ankon35 <ankon35744@gmail.com>
2025-10-28 06:58:49 +0600

    app create and create a endpoint

--- learnDjango/learnDjango/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, This is the Home Page")
-
-def about(request):
-    return HttpResponse("Hello, This is the About Page")
-
---
+++ learnDjango/views.py
@@ -0,0 +1,8 @@
+# from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse("Hello, This is the Home Page")
+
+# def about(request):
+#     return HttpResponse("Hello, This is the About Page")
+
